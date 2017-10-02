4d242848ebd5c9dbef16d4ce4291c517077fa8c4
Matt Ziegler <mattpziegler@gmail.com>
2017-10-02 01:49:45 -0500

    hide samples until you click button

--- src/app/result-study.component.ts
+++ src/app/result-study.component.ts
@@ -14,8 +14,16 @@ export class ResultStudyComponent {
   @Input()
   study: ResultStudy;
 
+  // Sample groups hidden until the user clicks "view samples" and then sampleGroupsVisible
+  // is set to true.  The first X number of sample groups are show, until the user clicks
+  // "see all samples" and then sampleGroupsExpanded becomes true and all are shown.
+  private sampleGroupsVisible = false;
   private INITIAL_SAMPLEGROUP_DISPLAY = 3;
   private sampleGroupsExpanded = false;
+
+  showSampleGroups(): void {
+    this.sampleGroupsVisible = true;
+  }
 
   expandSampleGroups(): void {
     this.sampleGroupsExpanded = true;
