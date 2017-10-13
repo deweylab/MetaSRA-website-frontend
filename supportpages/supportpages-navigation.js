21ae23386a1135f5966797521a4e9bc240d13ade
Matt Ziegler <mattpziegler@gmail.com>
2017-10-12 23:46:13 -0500

    integrated supporting pages

---
+++ supportpages/supportpages-navigation.js
@@ -0,0 +1,15 @@
+// This script just writes the navigation bar to the page, a makeshift
+// client-side include to keep the navication bar consistent between all
+// support pages.
+
+document.write('<nav class="top-nav nav d-flex align-items-center"><a class="nav-link" href="index"><img src="assets/logo.png" class="nav-logo"/></a><a class="nav-link" href="download.html">Download</a><a class="nav-link" href="faq.html">FAQ</a><a class="nav-link" href="users.html">Mailing List</a><a class="nav-link" href="publication.html">Publication</a><a class="nav-link" href="links.html">Links</a><a class="nav-link" href="contact.html">Contact</a></nav>');
+
+
+// Also google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-64529506-3', 'auto');
+ga('send', 'pageview');
